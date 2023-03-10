dcd0cc5a3d328706127df2bbb92d1cda175c9839
MerlinSheebaF <105285961+MerlinSheebaF@users.noreply.github.com>
2023-03-10 22:13:54 +0000

    initial commit to bmi calculator project

---
+++ js/script.js
@@ -0,0 +1,42 @@
+window.onload = () => {
+    let button = document.getElementById("calc");
+
+    button.addEventListener("click", calculateBMI);
+};
+
+function calculateBMI() {
+
+
+    let height = parseInt(document
+        .getElementById("height").value);
+
+
+    let weight = parseInt(document
+        .getElementById("weight").value);
+
+
+    let result = document.getElementById("result");
+
+
+    if (height === "" || isNaN(height))
+        result.innerHTML = "Provide a valid Height!";
+
+    else if (weight === "" || isNaN(weight))
+        result.innerHTML = "Provide a valid Weight!";
+
+    else {
+
+        let bmi = (weight / ((height * height)
+            / 10000)).toFixed(2);
+
+        if (bmi < 18.6) result.innerHTML =
+            `Under Weight : <span>${bmi}</span>`;
+
+        else if (bmi >= 18.6 && bmi < 24.9)
+            result.innerHTML =
+                `Normal : <span>${bmi}</span>`;
+
+        else result.innerHTML =
+            `Over Weight : <span>${bmi}</span>`;
+    }
+}
